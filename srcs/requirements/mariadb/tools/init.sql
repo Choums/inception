2ac2d24e7e857641b6c2d42f3afe45309e405cb4
chams <chams@Inception.myguest.virtualbox.org>
2023-04-12 20:37:25 +0200

    maria fonctionne test a faire

--- srcs/requirements/mariadb/tools/init.sql
+++ srcs/requirements/mariadb/tools/init.sql
@@ -21,20 +21,14 @@
 
 -- --# Refresh change
 -- FLUSH PRIVILEGES;
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
-	cat << EOF > /tmp/create_db.sql
-	USE mysql;
-	FLUSH PRIVILEGES;
-	DELETE FROM     mysql.user WHERE User='';
-	DROP DATABASE test;
-	DELETE FROM mysql.db WHERE Db='test';
-	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-	CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-	CREATE USER '$MYSQL_DB'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
-	GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_DB'@'%';
-	FLUSH PRIVILEGES;
-	EOF
-	/usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-fi
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_DB'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_DB'@'%';
+FLUSH PRIVILEGES;
