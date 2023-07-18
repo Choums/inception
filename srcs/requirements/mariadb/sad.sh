0781ba1307bcb335d4d96d0fa5c9fa047be01ec5
Choums <chams.aidel@outlook.fr>
2023-07-18 16:20:17 +0200

    removed sh test scripts

--- srcs/requirements/mariadb/sad.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker rm db
-docker build . --file Dockerfile -t mari
-docker run --name db mari
--- srcs/requirements/wordpress/sad.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker rm try1
-docker build . --file Dockerfile -t wp
-docker run --name try1 -it wp sh
